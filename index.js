3744ff309b8fa8856c60cd99b4d9a57d579397f5
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-03-11 16:30:36 +0200

    Update to handle express routing correctly in production

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const billingRoutes = require('./routes/billingRoutes');
 const keys = require('./config/keys');
@@ -23,6 +24,16 @@ app.use(passport.session());
 
 authRoutes(app);
 billingRoutes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assest
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
