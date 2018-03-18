01ab8ff3f8c29e8b17ae797b88e3074d942aa96b
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-03-18 16:08:25 +0200

    Minor refactor SurveyForm

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,17 +1,21 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' },
+];
+
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
-        <Field label="Survey Title" type="text" name="title" component={SurveyField} />
-        <Field label="Subject Line" type="text" name="subject" component={SurveyField} />
-        <Field label="Email Body" type="text" name="body" component={SurveyField} />
-        <Field label="Recipient List" type="text" name="emails" component={SurveyField} />
-      </div>
-    );
+    return _.map(FIELDS, ({ label, name }) => (
+      <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+    ));
   }
 
   render() {
@@ -25,6 +29,10 @@ class SurveyForm extends Component {
     );
   }
 }
+
+SurveyForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
 
 export default reduxForm({
   form: 'surveyForm',
