a6bd6ab540facf272cc8d6b02fe9a476dc34a6b3
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-03-19 13:42:20 +0200

    Refactor SurveyField, add props validation

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default ({ input, label }) => (
+const SurveyField = ({ input, label, meta: { error, touched } }) => (
   <div>
+    {console.log(this)}
     <label>{label}</label>
-    <input {...input} />
+    <input {...input} style={{ marginBottom: '5px' }} />
+    <div className="red-text" style={{ marginBottom: '20px' }}>
+      {touched && error}
+    </div>
   </div>
 );
+
+SurveyField.propTypes = {
+  input: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    onBlur: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onDragStart: PropTypes.func.isRequired,
+    onDrop: PropTypes.func.isRequired,
+    onFocus: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
+  }).isRequired,
+  label: PropTypes.string.isRequired,
+  meta: PropTypes.shape({
+    error: PropTypes.string.isRequired,
+    touched: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
+export default SurveyField;
