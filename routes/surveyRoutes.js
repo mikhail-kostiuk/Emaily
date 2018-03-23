0478d7eba8d2b6e6cbc43a939b0d4df86d5a4641
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-03-23 15:01:28 +0200

    Refactor the survey responses processing

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -15,19 +15,21 @@ module.exports = (app) => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      // either will be an object or null if method "test" couldn't extract data
-      const match = p.test(pathname);
-      if (match) {
-        return { email, surveyId: match.surveyId, choice: match.choice };
-      }
-    });
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        // either will be an object or null if method "test" couldn't extract data
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+        return null;
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
 
-    console.log(uniqueEvents);
+    console.log(events);
 
     res.send({});
   });
