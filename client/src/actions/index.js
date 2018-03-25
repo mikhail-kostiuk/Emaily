80800f9783aba1f6e4a9b46b186ba627f76e3c47
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-03-25 20:06:17 +0300

    Implement surveys overview dashboard

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './actionTypes';
+import { FETCH_USER, FETCH_SURVEYS } from './actionTypes';
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
@@ -17,4 +17,10 @@ export const submitSurvey = (values, history) => async (dispatch) => {
 
   history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async (dispatch) => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => (
   <div style={{ position: 'relative', minHeight: '200px' }}>
-    Dashboard
+    <SurveyList />
     <div className="fixed-action-btn" style={{ position: 'absolute', bottom: '0', right: '0' }}>
       <Link to="/surveys/new" className="btn-floating btn-large red">
         <i className="material-icons">add</i>
